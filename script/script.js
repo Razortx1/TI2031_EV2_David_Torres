3431a0dae5e19a83863a371247ac242ec5d213c0
Razortx1 <david.torres20022511@gmail.com>
2021-11-24 16:25:30 -0300

    Formulario completo v1.0

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
-/*Funcion para el modo oscuro*/
 function mode(){
+    /*Funcion para el modo oscuro*/
     document.querySelector('.body').classList.toggle('dark');
     boton = document.getElementById("boton");
     if (boton.innerText == 'Modo Oscuro'){
@@ -11,60 +11,133 @@ function mode(){
         boton.className = 'btn btn-dark';
     }
 };
-/*Funcion para validar ambas contraseñas*/
 function validarcontra(){
-    contra1 = document.getElementById("pass").value
-    contra2 = document.getElementById("repass").value
+    /*Funcion para validar ambas contraseñas*/
+    contra1 = document.getElementById("pass").value;
+    contra2 = document.getElementById("repass").value;
     if(contra1 != '' && contra2 != ''){
         if(contra1 == contra2){
-            expreg = /[0-9]/d
+            expreg = /[0-9]/d;
             if(expreg.test(contra1) == true){
-                expreg = /[a-z]/d
+                expreg = /[a-z]/d;
                 if(expreg.test(contra1) == true){
-                    expreg = /[A-Z]/d
+                    expreg = /[A-Z]/d;
                     if(expreg.test(contra1) == true){
-                        document.getElementById("warning").innerHTML = ""
+                        document.getElementById("warning").innerHTML = "";
+                        return contra1;
                     }
                     else{
-                        document.getElementById("warning").innerHTML = "Las contraseñas deben de tener almenos una mayuscula"
+                        document.getElementById("warning").innerHTML = "Las contraseñas deben de tener almenos una mayuscula";
+                        return ""
                     }
                 }
                 else{
-                    document.getElementById("warning").innerHTML = "Las contraseñas deben de tener almenos una minuscula"
+                    document.getElementById("warning").innerHTML = "Las contraseñas deben de tener almenos una minuscula";
+                    return ""
                 }
             }
             else{
-                document.getElementById("warning").innerHTML = "Las contraseñas deben tener almenos un numero"
+                document.getElementById("warning").innerHTML = "Las contraseñas deben tener almenos un numero";
+                return ""
             }
         }
         else{
-            document.getElementById("warning").innerHTML = 'Deben ser iguales ambas contraseñas'
+            document.getElementById("warning").innerHTML = 'Deben ser iguales ambas contraseñas';
+            return ""
         }
     }
     else{
-        document.getElementById("warning").innerHTML = "Las contraseñas estan vacias"
+        document.getElementById("warning").innerHTML = "Las contraseñas estan vacias";
+        return ""
     }
 };
 function validarrut(){
+    /*Funcion para validar que el input no este vacio, y comprobar si el rut es valido o no */
+    document.getElementById("rutval").innerHTML = "";
     let rut = document.getElementById("rut").value;
-    let aux = rut.split("-");
-    let fr = aux[0];
-    let ut = aux[1];
-    let rutinver = fr.split("").reverse();
-    let acumulador = 0;
-    let multiplicador = 2;
-    for(let numero of rutinver){
-        acumulador += parseInt(numero) * multiplicador;
-        multiplicador++
-        if(multiplicador==8){
-            multiplicador = 2
+    if(rut != ""){
+        let aux = rut.split("-");
+        let fr = aux[0];
+        let ut = aux[1];
+        let rutinver = fr.split("").reverse();
+        let acumulador = 0;
+        let multiplicador = 2;
+        for(let numero of rutinver){
+            acumulador += parseInt(numero) * multiplicador;
+            multiplicador++
+            if(multiplicador==8){
+                multiplicador = 2;
+            }
+        }
+        let digitocalculado = 11 - (acumulador%11);
+        if(digitocalculado == ut){
+            console.log("El rut es valido");
+            return rut;
+        }
+        else{
+            console.log("El rut no es valido");
+            return ""
         }
     }
-    let digitocalculado = 11 - (acumulador%11)
-    if(digitocalculado == ut){
-        console.log("El rut es valido")
+    else{
+        document.getElementById("rutval").innerHTML = "La pestaña de rut esta vacia";
+        return ""
+    }
+    
+}
+function validarname(){
+    /*Funcion para validar que el input name no este vacio*/
+    document.getElementById("nameval").innerHTML = ""
+    let name = document.getElementById("name").value;
+    if (name != ""){
+        return name
     }
     else{
-        console.log("El rut no es valido")
+        document.getElementById("nameval").innerHTML = "El nombre esta vacio"
+        return "";
+    }
+}
+function validarapell(){
+    /*Funcion para validar que el input last_name no este vacio*/
+    document.getElementById("lastnameval").innerHTML = ""
+    let last_name = document.getElementById("last_name").value;
+    if(last_name != ""){
+        return last_name;
+    }
+    else{
+        document.getElementById("lastnameval").innerHTML = "El Apellido esta vacio";
+        return "";
+    }
+}
+
+function validarmail(){
+    /*Funcion para validar que el input mail no este vacio*/ 
+    document.getElementById("mailval").innerHTML = ""
+    let mail = document.getElementById("email").value;
+    if(mail != ""){
+        return mail;
+    }
+    else{
+        document.getElementById("mailval").innerHTML = "El correo esta vacio"
+        return "";
+    }
+}
+function llamar(){
+    /*Funcion para llamar a las demas funciones */
+    rut = validarrut();
+    nombre = validarname();
+    apellido = validarapell();
+    mail = validarmail();
+    contraseña = validarcontra();
+    if(rut != "" && nombre != "" && apellido != "" && mail != "" && contraseña != ""){
+        console.log(rut);
+        console.log(nombre);
+        console.log(apellido);
+        console.log(mail);
+        console.log(contraseña)
+        alert("El Formulario fue enviado exitosamente")
+    }
+    else{
+        alert("El Formulario no pudo ser enviado por varios errores")
     }
 }
